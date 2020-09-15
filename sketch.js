9f76209688578f002c6b41c7ae2a546b35cab080
Pratyush21706 <65002484+Pratyush21706@users.noreply.github.com>
2020-09-15 18:55:40 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,25 +1,35 @@
-localStorage.ghus;
-var food,water,sleep,run;
+localStorage.speed = 1 ;
+var food,water,sleep,run,qot,nm;
 localStorage.name;
 localStorage.age;
-var a = 1,b=1,quotes;
+var talkbox;
+var rand;
+var a = 2,b=1,s = 1;
+            var count =1,karna = 1,h =900;
+;
+var option1 = false;
+var col1="black",col2 = "col1";
+
 var nameInput;
 function preload(){
   smile =loadImage("intro.png")
     logo = loadImage("logo.jpg")
     user = loadImage("user.jpg")
         age = loadImage("age.jpg")
-    homebg = loadImage("homebg.png")
+            bot = loadImage("bot.png")
+dk = loadImage("desktop.png")
+    light = loadImage("light.png")
+    dark = loadImage("dark.png")
     food = loadImage("food.png")
     water = loadImage("water.png")
     sleep = loadImage("sleep.png")
     run = loadImage("run.png")
-        heart = loadImage("heart.png")
-    goal = loadImage("goal.png");
-        report = loadImage("report.png")
+    qot = loadImage("qot.png")
+    
 }
 function setup(){
-          createCanvas(windowWidth, windowHeight);
+          createCanvas(windowWidth/1.01, windowHeight);
+    nm = window.innerHeight/17
 col = color(255,255,255,0)
     nextButton = createButton(`→`)
     nextButton.position(width/2.1,-555)
@@ -32,7 +42,7 @@ col = color(255,255,255,0)
     input1.position(width/4,-555);
     input1.style(`font-size`,`20px`)
         input1.style(`border`,`200px`)
-//    input1.style(`bottom border`,`20px solid black`)
+//    input1.style(`bottom border`,`20px solid col1`)
 //        input1.style(`padding`,`5px`)
                 input1.style(`outline`,`0px`)
 
@@ -41,7 +51,7 @@ col = color(255,255,255,0)
     input2.position(width/4,-555);
     input2.style(`font-size`,`20px`)
         input2.style(`border`,`200px`)
-//    input1.style(`bottom border`,`20px solid black`)
+//    input1.style(`bottom border`,`20px solid col1`)
 //        input2.style(`padding`,`15px`)
             input2.style(`outline`,`0px`)
 
@@ -63,35 +73,147 @@ radio = createRadio();
                 registerButton.style(`padding-top`,`15px`)
                     registerButton.style(`padding-bottom`,`15px`)
                         registerButton.style(`border-radius`,`10px`)
+                         registerButton.style(`box-shadow`,`0px 15px 20px rgba(46, 229, 157, 0.9)`)
     registerButton.mousePressed(registerd)
+    
 //let am = window.innerWidth+"px";
 //    console.log(am)
-   profile = createButton(`.`)
-profile.style(`background-color`,`aqua`);
-profile.position(width/6,-555)
-profile.style(`border`,`2px`)
-   profile.style(`border-radius`,`15px`)
-  profile.style(`padding-left`,`100px`)
-           profile.style(`padding-right`,`225px`)
-               profile.style(`padding-top`,`85px`)
-                   profile.style(`padding-bottom`,`5px`)
-//    loadJSON("https://favqs.com/api/qotd",loaded)
+   Health = createButton(`.`)
+Health.style(`background-color`,col);
+Health.position(15,-555)
+Health.style(`border`,`2px`)
+   Health.style(`border-radius`,`15px`)
+  Health.style(`padding-left`,`15px`)
+           Health.style(`padding-right`,`160px`)
+               Health.style(`padding-top`,`50px`)
+                   Health.style(`padding-bottom`,`10px`)
+    
+     Goals = createButton(`.`)
+Goals.style(`background-color`,col);
+Goals.position(width/1.95,-555)
+Goals.style(`border`,`2px`)
+   Goals.style(`border-radius`,`15px`)
+  Goals.style(`padding-left`,`20px`)
+           Goals.style(`padding-right`,`160px`)
+               Goals.style(`padding-top`,`50px`)
+                   Goals.style(`padding-bottom`,`10px`)
+    
+Bot= createButton(`.`)
+Bot.style(`background-color`,col);
+    Bot.mousePressed(bring)
+Bot.position(15,-555)
+Bot.style(`border`,`2px`)
+Bot.style(`border-radius`,`15px`)
+Bot.style(`padding-left`,`20px`)
+Bot.style(`padding-right`,`160px`)
+Bot.style(`padding-top`,`50px`)
+Bot.style(`padding-bottom`,`10px`)
+    
+    Reports= createButton(`.`)
+Reports.style(`background-color`,col);
+Reports.position(width/1.95,-555)
+Reports.style(`border`,`2px`)
+Reports.style(`border-radius`,`15px`)
+Reports.style(`padding-left`,`20px`)
+Reports.style(`padding-right`,`160px`)
+Reports.style(`padding-top`,`50px`)
+Reports.style(`padding-bottom`,`10px`)
+    
+     talkbox = createInput(``).attribute(`placeholder`,`    Introduction to Ambibot`)
+    talkbox.position(width/1.95,-555)
 
+    talkbox.style(`font-size`,`28px`)
+  talkbox.style(`padding-right`,`115px`)
+talkbox.style("border","40px")
+    talkbox.style(`border-radius`,`100px`)
+            talkbox.style(`outline`,`0px`)
+    talkbox.style(`background-color`,`#E5E5E5`);
+    
+    send = createButton(``)
+    send.position(width/1.133,-555)
+    send.mousePressed(tell)
+           
+    send.style(`background`,`url("send.png")`);
+    send.style(`background-size`,`100%`)
+//    send.style(`font-size`,`3-px`)
+    send.style(`border`,`10000px`)
+//        send.style(`border-radius`,`1500px`)
+//    send.style(`padding-right`,`60px`)
+//        send.style(`padding-bottom`,`350px`)
+//        send.style(`padding-top`,`8px`)
+    send.style(`padding`,`14px`)
+                send.style(`outline`,`0px`)
+    
+     buttonA = createButton(`Book A Doctor`)
+    buttonA.position(width/1.133,-555)
+    buttonA.mousePressed(tell)
+           
+    buttonA.style(`background-color`,`#3744DE`);
+    buttonA.style(`background-size`,`100%`)
+//    send.style(`font-size`,`3-px`)
+    buttonA.style(`border`,`10000px`)
+buttonA.style(`padding`,`14px`)
+                buttonA.style(`outline`,`0px`)
+      buttonA.style(`font-size`,`20px`)
+    buttonA.style(`color`,`white`)
+buttonA.style(`border-radius`, `15px`)
+    
+     
+     buttonB = createButton(`Medical Tests`)
+    buttonB.position(width/1.133,-555)
+    buttonB.style(`font-size`,`20px`)
+    buttonB.style(`color`,`white`)
+    buttonB.mousePressed(tell)
+     buttonB.style(`background-color`,`#3744DE`);
+    buttonB.style(`background-size`,`100%`)
+    buttonB.style(`border-radius`, `15px`)
+//    send.style(`font-size`,`3-px`)
+    buttonB.style(`border`,`10000px`)
+buttonB.style(`padding`,`14px`)
+                buttonB.style(`outline`,`0px`)
+
+
+  see = createButton(``)
+    see.position(width/1.133,-5555)
+    see.style(`font-size`,`20px`)
+    see.style(`color`,`white`)
+see.style(`background-color`,`rgba(255, 255, 255, 0.7)`);
+    see.style(`border`,`0px`)
+    see.style(`background-size`,`100%`)
+    see.style(`border-radius`, `15px`)
+    see.style(`padding-bottom`,`530px`)
+        see.style(`padding-right`,`480px`)
+    
+      backButton = createButton(`←`)
+    backButton.position(width/2.1,-555)
+    backButton.style(`background`,`transparent`);
+    backButton.style(`font-size`,`30px`)
+    backButton.style(`border`,`0px`)
+    backButton.mousePressed(back)
+
+//see.style(`padding`,`240px`)
+                see.style(`outline`,`0px`)
+    nextButton.mousePressed(next)
+//    loadJSON("https://favqs.com/api/qotd",loaded)
+  
 }
 //function loaded(data){
 //    quotes = data;
 //    console.log(quotes)
 //}
+function bring(){
+    localStorage.speed = 35;
+}
 function registerd(){
   localStorage.name = input1.value();
     localStorage.age = input2.value();
     console.log("Name: "+localStorage.name)
         console.log("Age: "+localStorage.age)
-    localStorage.ghus = 34;
+    localStorage.speed = 34;
  input1.position(width/4,-555);
  input2.position(width/4,-555);
  registerButton.position(width/2.1,-555)
-     alert(`You Have Registerd Successfully`)                     
+//     alert(`You Have Registerd Successfully`)                     
 }
 function next(){
     a = 2;
@@ -102,6 +224,7 @@ function draw(){
 //        alert('please rotate your phone')
 //    }
     drawSprites();
+  
 //    if(quotes){
 //        b = 2;
 //    }
@@ -109,9 +232,10 @@ function draw(){
 //        better = quotes.body;
 //        console.log(better)
 //    }
- if(localStorage.ghus==null){
+ if(localStorage.speed==1){
      if(a===1){
-    fill("black")
+         
+    fill("col1")
           image(smile,width/11,height/8,width/1.1,height/3)
     textSize(60)
      textStyle("bold")
@@ -132,6 +256,7 @@ rect(width/1.9,height/1.28,width/9,3)
      if(a===2){
              nextButton.position(width/2.1,-555)
              input1.position(width/4.5,height/2);
+         
     input2.position(width/4.5,height/1.65);
 fill("gray")
            noStroke();
@@ -145,87 +270,206 @@ rect(width/8,height/1.82,width/1.3,1)
 
      }
 }
-    if(localStorage.ghus==34){
-         resizeCanvas(window.innerWidth,window.innerHeight*1.1)
+    if(localStorage.speed==34){
+    if(deviceOrientation ==="landscape"){
+        alert("rotate your device")
+    }
 
-    background(homebg)
-        fill("white")
-        textSize(20);
-       go=  text("Good Morning, ",width/5,height/7.05)
-       text("There is no NOBILITY in fighting",width/6,height/5.2)
-        textStyle("bold")
-        text(localStorage.name+"!!",width/1.9,height/7)
-//        profile.position(width/5,12)
-        noFill();
-        stroke("gray")
-        strokeWeight(0.05)
-        rect(15,height/2.6,width/1.1,height/12)
-                rect(15,height/2.1,width/1.1,height/12)
-                        rect(15,height/1.75,width/1.1,height/12)
-//                                 strokeWeight(1.1)
-        rect(15,height/1.5,width/1.1,height/12)
-       
+        if(window.innerWidth>550){
+                     resizeCanvas(window.innerWidth,window.innerHeight)
+            background(dk)
+            textSize(width/30);
+//            textStyle("bold");
+            fill("white")
+            text(localStorage.name,width/12,height/12)
+Health.style(`padding-left`,`15px`)
+           Health.style(`padding-right`,`280px`)
+               Health.style(`padding-top`,`80px`)
+                   Health.style(`padding-bottom`,`65px`)
+ Goals.style(`padding-left`,`20px`)
+           Goals.style(`padding-right`,`280px`)
+               Goals.style(`padding-top`,`80px`)
+                   Goals.style(`padding-bottom`,`65px`)
+Bot.style(`padding-left`,`20px`)
+Bot.style(`padding-right`,`280px`)
+Bot.style(`padding-top`,`80px`)
+Bot.style(`padding-bottom`,`65px`)
+Reports.style(`padding-left`,`20px`)
+Reports.style(`padding-right`,`280px`)
+Reports.style(`padding-top`,`80px`)
+Reports.style(`padding-bottom`,`65px`)
+            Health.position(width/3.5,height/1.4)
+Goals.position(width/1.32,height/1.4)
+Bot.position(width/20,height/1.4)
+Reports.position(width/1.91,height/1.4)
+    
+            desktop();
+            console.log("wjhdw")
+      
+                
+
+        }else{
+                     resizeCanvas(window.innerWidth,window.innerHeight*1.1)
+
+            background(dark)
+             fill("white")
+        textSize(width/20);
+//       text("There is no NOBILITY in fighting",width/6,height/5.2)
+//                     textStyle("bold")
+        text(localStorage.name,width/5,height/16.5)
+//   noFill();
+//        stroke("gray")
+//        strokeWeight(0.05)
+//        rect(15,height/2.6,width/1.1,height/12)
+//                rect(15,height/2.1,width/1.1,height/12)
+//                        rect(15,height/1.75,width/1.1,height/12)
+////                                 strokeWeight(1.1)
+//        rect(15,height/1.5,width/1.1,height/12)
+
 //         profile.position(width/12,height/5.4)
 
-        fill("black");
-        
-        textSize(18);
-text("Health",width/5.4,height/1.2)
-text("Monitoring",width/5.4,height/1.16)
-text("Goals",width/1.35,height/1.18)
-text("Dr. Bot",width/5,height/1.05)
-text("Reports",width/1.3,height/1.05)
-textSize(20);
-text("Food",width/5,height/2.35)
-text("Water",width/5,height/1.95)
-text("Sleep",width/5,height/1.62)
-text("Activity",width/5,height/1.4)
 
 
+//                 textStyle("normal")
+//                 textSize(width/36)
+//                 text("0 out of 1800 cals consumed",width/5,height/2.2)
+//                                  text("0 out of 8 glasses ",width/5,height/1.85)
+//                                  text("0 out of 8 hours ",width/5,height/1.55)
+//                                  text("Active for 0 hours ",width/5,height/1.35)
+//
+//      
+////                    image(sample,width/2.5,6,width/5,height/10)
+//                           image(food,17,height/2.55,width/8,height/15)
+//                                                      image(water,17,height/2.06,width/8,height/15)
+//                                                      image(sleep,20,height/1.74,width/8,height/15)
+//                                                      image(run,17,height/1.49,width/8,height/15)
+//                                                      image(qot,15,height/5.2,width/1.1,height/6.8)
+//          textSize(width/20)
+//text("Today's Status",8,height/2.7)
+//
+//        noFill();
+//        stroke("white")
+//        strokeWeight(3)
+//        circle(width/2,height/17,width/5,height/40)
+//        
+//         fill("red")
+//         circle(width/2,height/17,width/5,height/40)
+//         fill("green")
+//        
+//         fill("white")
+//textSize(width/27);
+//textStyle("bold")
+//        noStroke();
+//text("There is no NOBILITY in fighting",width/7,height/4)
+//text("Sometimes it is best to SMILE and",width/7,height/3.6)
+//text("move on",width/2.5,height/3.25)
+count = 0;
+            Health.position(15,height/1.26)
+Goals.position(width/1.95,height/1.26)
+Bot.position(15,height/1.11)
+Reports.position(width/1.95,height/1.11)
+        }
 
-                 textStyle("normal")
-                 textSize(18)
-                 text("0 out of 1800 cals consumed",width/5,height/2.2)
-                                  text("0 out of 8 glasses ",width/5,height/1.85)
-                                  text("0 out of 8 hours ",width/5,height/1.55)
-                                  text("Active for 0 hours ",width/5,height/1.35)
-
-        textSize(25)
-text("Today's Status",8,height/2.7)
-
-//                    image(sample,width/2.5,6,width/5,height/10)
-                           image(food,17,height/2.55,width/8,height/15)
-                                                      image(water,17,height/2.06,width/8,height/15)
-                                                      image(sleep,20,height/1.74,width/8,height/15)
-                                                      image(run,17,height/1.49,width/8,height/15)
-                                                      image(heart,17,height/1.24,width/8,height/15)
-                                                      image(goal,width/1.7,height/1.24,width/8,height/15)
-
-                                                      image(report,width/1.7,height/1.1,width/8,height/15)
-
-        noFill();
-        stroke("white")
-        strokeWeight(3)
-        circle(width/2,height/17,width/5,height/40)
-        
-         fill("red")
-         circle(width/2,height/17,width/5,height/40)
-         fill("green")
-         fill("aqua")
-         circle(width/6,height/4.29,width/5,height/40)
-         circle(width/1.15,height/3.5,width/5,height/40)
-fill("aqua")
-                 noStroke();
-                rect(width/11,height/5,width/1.2,height/8)
-
-         fill("black")
-textSize(18);
-textStyle("bold")
-text("There is no NOBILITY in fighting",width/7,height/4)
-text("Sometimes it is best to SMILE and",width/7,height/3.6)
-text("move on",width/2.5,height/3.25)
-
-
+    
+         buttonA.position(width/9,-555);
+                                buttonB.position(width/2,-555);
+        backButton.position(200,-555);
+        talkbox.position(200,-555);
+       
+      
+//        profile.position(width/5,12)
+     
+see.position(200,-5555)
     }
 //console.log(localStorage.login)
+        if(localStorage.speed==35){
+         resizeCanvas(window.innerWidth,window.innerHeight)
+Health.position(15,-555)
+Goals.position(width/1.95,-555)
+Bot.position(15,-555)
+Reports.position(width/1.95,-555)
+    background(bot)
+            
+            backButton.position(15,3)
+             fill("white")
+            rect(0,0,width,height/15)
+            strokeWeight(0.2)
+            fill(col1)
+            textSize(width/27)
+            textStyle("bold")
+            text("Ambibot",width/2.5,height/25)
+           
+            
+ talkbox.position(20,height/1.08);
+fill("#3744DE")
+    if(karna===1){
+            count++
+    }
+
+            if(count>15){
+                fill("#3744DE")
+            noStroke();     
+rect(width/19,height/15,width/3,height/20)
+                textSize(width/40);
+                console.log(width/30)
+                   fill("white")
+                text("Hello "+localStorage.name,width/12,height/10)
+            }
+              if(count>25){
+                fill("#3744DE")
+            noStroke();     
+rect(width/19,height/8,width/2,height/8.5)
+                textSize(width/38);
+                   fill("white")
+                  
+                text("I am `Ambibot` your virtual guide",width/12.5,height/6.5)
+                                  text("for all your health related queries",width/12.5,height/5.5)
+                                                    text("and a coach for you to live a fit and  ",width/12.5,height/4.8)
+                   text("a Healthy Life",width/12.5,height/4.25)
+
+
+            }
+              if(count>35){
+                    fill(col1)
+    textSize(width/30)
+    text(window.innerWidth,width/2,height/12)
+                fill("#3744DE")
+            noStroke();     
+rect(width/19,height/3.9,width/1.8,height/20)
+                textSize(width/width*15);
+                   fill("white")
+                text("Tell me how can I help you "+localStorage.name,width/12,height/3.45)
+            }
+            if(count>40){
+                buttonA.position(width/9,height/3);
+                                buttonB.position(width/2,height/3);
+
+            }
+            
+            if(count>40&&karna===1){
+                talkbox.attribute(`placeholder`,`Choose One of the options`)
+//                 send.position(width/1.2,height/1.075)
+            }
+            if(option1){
+see.position(16,height/10)
+                talkbox.position(200,-555)
+            }
+        }
+}
+
+function tell()
+{
+    count = 0;
+    karna = 2;
+     buttonA.position(width/9,-555);
+                                buttonB.position(width/2,-555);
+  option1 = true;
+    
+    }
+function desktop()
+{
+}
+
+function back(){
+    localStorage.speed=34;
 }
